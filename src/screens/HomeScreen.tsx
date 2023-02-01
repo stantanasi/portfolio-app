ecd2d615effce61adc5e4b78c9f9e42a8053a7cc
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-01 14:51:31 +0100

    feat(home-screen): display profile data

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,19 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://avatars.githubusercontent.com/u/46174829?v=4',
+          }}
+          style={styles.profilePic}
+        />
+        <Text style={styles.name}>Lory-Stan TANASI</Text>
+        <Text style={styles.headline}>Développeur full stack mobile/web chez 42c</Text>
+      </View>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
+  header: {},
+  profilePic: {
+    width: 150,
+    height: 150,
+  },
+  name: {},
+  headline: {},
 })
