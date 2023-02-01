5caece1eb176ae5db4153e992229ab71ef6fe339
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-01 16:20:22 +0100

    feat(education-section): display educations

--- src/components/education/EducationSection.tsx
+++ src/components/education/EducationSection.tsx
@@ -1,12 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
+import Education from './Education'
+import educations from '../../data/educations'
 
 export default function EducationSection() {
   return (
-    <View>
-      <Text>EducationSection</Text>
+    <View style={styles.container}>
+      {educations.map((education) => (
+        <Education education={education} />
+      ))}
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {},
+})
