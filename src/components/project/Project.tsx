95faedd075da6574a4f622e39cdf3a162c30fa7b
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-01 16:33:33 +0100

    feat: create Project component

---
+++ src/components/project/Project.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function Project() {
+  return (
+    <View>
+      <Text>Project</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
