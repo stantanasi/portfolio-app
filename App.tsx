650e2f351190b3630b351c5bd2eb8c0d29302a4c
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-01 14:29:51 +0100

    feat(app): add HomeScreen to navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet } from 'react-native';
+import HomeScreen from './src/screens/HomeScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
