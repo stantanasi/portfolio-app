bb521ef115cc24f5d81017015573e8ad417a4429
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-01 16:14:18 +0100

    feat(educations): add Estiam education data

--- src/data/educations.ts
+++ src/data/educations.ts
@@ -8,6 +8,16 @@ export interface IEducation {
   description: string;
 }
 
-const educations: IEducation[] = [];
+const educations: IEducation[] = [
+  {
+    school: 'Éstiam • École supérieure des technologies de l\'information appliquées aux métiers',
+    degree: '',
+    fieldOfStudy: '',
+    startDate: '2020-10',
+    endDate: '2023-09',
+    grade: '',
+    description: '',
+  },
+];
 
 export default educations
