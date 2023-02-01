f6518b15945af28bbe821148cc1f01a471ba3fe3
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-01 17:15:36 +0100

    feat(experiences): add experiences data

--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -10,6 +10,24 @@ const experiences: IExperience[] = [
     endDate: null,
     description: '',
   },
+  {
+    title: 'Stagiaire en électronique-informatique',
+    employementType: 'internship',
+    companyName: 'Leviathan Dynamics',
+    location: 'La Courneuve, Île-de-France, France',
+    startDate: '2020-05',
+    endDate: '2020-07',
+    description: 'Programmation d\'un automate industriel connecté\nConception de l\'Interface Homme-Machine\n\nPrise en main de l\'automate programmable industriel (marque Delta Electronics)\nRéalisation de la structure du programme sous langage Ladder\nMise à jour du programme en fonction des retours d\'expérience terrain et ajout de nouvelles fonctionnalités',
+  },
+  {
+    title: 'Agent technique',
+    employementType: 'contract',
+    companyName: 'Centre Aquatique',
+    location: 'Levallois-Perret, Île-de-France, France',
+    startDate: '2018-08',
+    endDate: '2018-08',
+    description: 'Entretien de la piscine municipale (vestiaires, bureaux, escaliers)\nGestion des déchets\nVitrerie',
+  },
 ];
 
 export default experiences
