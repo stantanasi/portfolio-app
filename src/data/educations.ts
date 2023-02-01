b7ed1f52c6159bd56fff702402c8fd3b36375bcd
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-01 17:15:48 +0100

    feat(educations): add educations data

--- src/data/educations.ts
+++ src/data/educations.ts
@@ -10,6 +10,24 @@ const educations: IEducation[] = [
     grade: '',
     description: '',
   },
+  {
+    school: 'IUT de Cachan',
+    degree: 'DUT',
+    fieldOfStudy: 'Génie électrique et informatique industrielle',
+    startDate: '2018-09',
+    endDate: '2020-06',
+    grade: '',
+    description: '',
+  },
+  {
+    school: 'Lycée Léonard de Vinci',
+    degree: 'Baccalauréat Général série Scientifique',
+    fieldOfStudy: 'Sciences de l\'Ingénieur',
+    startDate: '2015-09',
+    endDate: '2018-06',
+    grade: 'Mention Assez Bien',
+    description: 'Baccalauréat Général série Scientifique, option Sciences de l\'Ingénieur, spécialité Informatique et Sciences du Numérique',
+  },
 ];
 
 export default educations
