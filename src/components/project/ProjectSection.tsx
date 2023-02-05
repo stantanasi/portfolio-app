394f1660cca045615dc7995b013d45714571b561
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-05 19:40:22 +0100

    feat(project-section): display loading indicator on projects loading

--- src/components/project/ProjectSection.tsx
+++ src/components/project/ProjectSection.tsx
@@ -1,26 +1,38 @@
-import { StyleSheet, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import { IProject } from '../../types/project.type';
 import Project from './Project';
 
 export default function ProjectSection() {
+  const [isLoading, setIsLoading] = useState(false)
   const [projects, setProjects] = useState<IProject[]>([]);
 
   useEffect(() => {
+    setIsLoading(true);
+
     fetch('https://api.github.com/users/stantanasi/repos?sort=pushed&direction=desc')
       .then((response) => response.json())
-      .then((data) => setProjects(data))
-  })
+      .then((data) => {
+        setProjects(data)
+        setIsLoading(false)
+      })
+  }, [])
 
   return (
     <View style={styles.container}>
       {projects.map((project, index) => (
         <Project key={index} project={project} />
       ))}
+      {isLoading && (
+        <View style={styles.loading}>
+          <ActivityIndicator size="large" />
+        </View>
+      )}
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {},
+  loading: {},
 })
