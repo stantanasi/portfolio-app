c170edf8404e206648195f090f28df89dc3720dd
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-05 19:41:00 +0100

    feat(education): display more education infos

--- src/components/education/Education.tsx
+++ src/components/education/Education.tsx
@@ -9,11 +9,26 @@ interface IProps {
 export default function Education({ education }: IProps) {
   return (
     <View style={styles.container}>
-      <Text>{education.school}</Text>
+      <Text style={styles.school}>{education.school}</Text>
+      <Text style={styles.degree}>{education.degree}</Text>
+      <Text style={styles.dates}>{education.startDate} - {education.endDate}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  school: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  degree: {
+    color: '#000',
+  },
+  dates: {
+    color: '#888',
+  },
 })
