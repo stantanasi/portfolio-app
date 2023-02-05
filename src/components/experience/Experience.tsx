93e1612e2603ba5aa18bfa73058080e0029ddfd1
Lory-Stan TANASI <stantanasi@gmail.com>
2023-02-05 19:41:16 +0100

    feat(experience): display more experience infos

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -9,12 +9,26 @@ interface IProps {
 export default function Experience({ experience }: IProps) {
   return (
     <View style={styles.container}>
-      <Text>{experience.title}</Text>
-      <Text>{experience.companyName} • {experience.employementType}</Text>
+      <Text style={styles.title}>{experience.title}</Text>
+      <Text style={styles.subtitle}>{experience.companyName} • {experience.employementType}</Text>
+      <Text style={styles.dates}>{experience.startDate} - {experience.endDate}</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    marginHorizontal: 20,
+    marginVertical: 10,
+  },
+  title: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    color: '#000',
+  },
+  dates: {
+    color: '#888'
+  },
 })
